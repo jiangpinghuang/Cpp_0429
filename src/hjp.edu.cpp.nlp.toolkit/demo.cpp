68f9f8bd7b9c95473defb4c773f4b7d1cc00425a
hjpwhu <hjpwhu@qq.com>
2016-12-22 07:43:12 +0800

    Create a Cpp project and initial commit.

---
+++ src/hjp.edu.cpp.nlp.toolkit/demo.cpp
@@ -0,0 +1,20 @@
+/*
+ * demo.cpp
+ *
+ *  Created on: Dec 22, 2016
+ *      Author: hjp
+ */
+
+# include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	std::cout << "Hello, CPP demo!" << std::endl;
+
+	return 0;
+}
+
+
+
